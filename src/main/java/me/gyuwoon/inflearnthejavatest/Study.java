1b23e0581cb783ad78190e1110a74bca47fbd336
gwjung <charminggw@gmail.com>
2020-11-16 06:28:40 +0900

    initial commit

---
+++ src/main/java/me/gyuwoon/inflearnthejavatest/Study.java
@@ -0,0 +1,5 @@
+package me.gyuwoon.inflearnthejavatest;
+
+public class Study {
+
+}
---
+++ src/test/java/me/gyuwoon/inflearnthejavatest/StudyTest.java
@@ -0,0 +1,14 @@
+package me.gyuwoon.inflearnthejavatest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StudyTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
