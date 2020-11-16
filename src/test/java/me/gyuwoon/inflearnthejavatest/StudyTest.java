e4de51ec359855234be076b53f4d921a25b90fd6
gwjung <charminggw@gmail.com>
2020-11-17 06:40:13 +0900

    익셉션 확인하기 (assertThrows)

--- src/test/java/me/gyuwoon/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/gyuwoon/inflearnthejavatest/StudyTest.java
@@ -1,9 +1,7 @@
 package me.gyuwoon.inflearnthejavatest;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -20,23 +18,12 @@ class StudyTest {
 	@Test
 	@DisplayName("스터디 만들기 😎")
 	void create_new_study() {
-		Study study = new Study(-10);
-		
-		// 세 개의 assert문을 assertAll 안에 람다식으로 묶은 것. (Executable 3개를 파라미터로 전달)
-		// 세 개를 한 번에 실행해서 뭐가 깨지는지 한 번에 알 수 있다. 
-		assertAll(
-				() -> assertNotNull(study),
-				() ->  assertEquals(StudyStatus.DRAFT, study.getStatus(),
-						() -> "스터디를 처음 만들면 "+ StudyStatus.DRAFT+" 상태다"),
-				() -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.")
-				);
-		
-		// 두 assert가 다 깨져있어도 실행 시 첫번째 assert만 깨지고 종료된다 -> 두번째 assert문은 첫번째를 고쳐야 확인 가능
-		// => 한 번에 실행하는 방법 ?? assertAll
-//		assertEquals(StudyStatus.DRAFT, study.getStatus(),
-//				()->  "스터디를 처음 만들면 "+ StudyStatus.DRAFT+" 상태다");
-//		// limit이 0이 아니라고 가정.
-//		assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.");
+		// Exception이 발생하는 것을 확인하는 방법 => assertThrows
+		// assertThrows(어떤 익셉션이 발생하는지, 어떤 코드를 실행할 때)
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+		// 발생하는 에러메시지를 받아서 내가 기대했던 메시지와 같은지 확인
+		String message = exception.getMessage();
+		assertEquals("limit은 0보다 커야 한다.", message);
 	}
 
 	@Test
