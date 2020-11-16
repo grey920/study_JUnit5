dc2c8cbfac7b2a04f44cba333d6ad36bfc91cf92
gwjung <charminggw@gmail.com>
2020-11-17 08:33:24 +0900

    @EnabledOnOS() : OS에 특화된 테스트 수행하기

--- src/test/java/me/gyuwoon/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/gyuwoon/inflearnthejavatest/StudyTest.java
@@ -1,8 +1,6 @@
 package me.gyuwoon.inflearnthejavatest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assumptions.assumeTrue;
-import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -12,6 +10,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 /* 조건에 따라 테스트 수행하기  - OS, 자바버전, 환경변수 등등 : assume*/
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -19,30 +20,18 @@ class StudyTest {
 
 	@Test
 	@DisplayName("스터디 만들기 😎")
+	@EnabledOnOs({OS.MAC, OS.LINUX}) // 특정한 운영체제일 때 실행한다
 	void create_new_study() {
 		// 시스템에서 환경변수를 꺼낸다 -> 테스트 환경이 로컬인 경우에만 다음에 있는 테스트를 수행한다.
 		String test_env = (System.getenv("TEST_ENV"));
-		System.out.println(test_env);
-		assumeTrue("LOCAL".equalsIgnoreCase(test_env));
-
-		assumingThat("LOCAL".equalsIgnoreCase(test_env), () -> {
-			// 환경이 LOCAL일때 실행
-			Study actual = new Study(100);
-			assertThat(actual.getLimit()).isGreaterThan(0);
-		});
-
-		assumingThat("gyuwoon".equalsIgnoreCase(test_env), () -> {
-			// 환경이 gyuwoon일때 실행
-			Study actual = new Study(10);
-			assertThat(actual.getLimit()).isGreaterThan(0);
-		});
-
-		Study actual = new Study(10);
+		System.out.println("local");
+		Study actual = new Study(100);
 		assertThat(actual.getLimit()).isGreaterThan(0);
 	}
 
 	@Test
 	@DisplayName("스터디 만들기 😃✨ ")
+	@DisabledOnOs(OS.MAC ) // 특정 운영체제일때에는 실행하지 않는다.
 	void create_new_study_again() {
 		System.out.println("create1");
 	}
