71040ed535a7a93e3924e860a083e04f9cef83da
gwjung <charminggw@gmail.com>
2020-11-23 06:33:57 +0900

    인자 값 조합 (ArguementsAccessor)를 이용해서 반복 테스트하기

--- src/test/java/me/gyuwoon/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/gyuwoon/inflearnthejavatest/StudyTest.java
@@ -16,7 +16,12 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledOnJre;
 import org.junit.jupiter.api.condition.JRE;
+import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
 import org.junit.jupiter.params.converter.ArgumentConversionException;
 import org.junit.jupiter.params.converter.ConvertWith;
 import org.junit.jupiter.params.converter.SimpleArgumentConverter;
@@ -56,13 +61,33 @@ class StudyTest {
     /* ParameterizedTest다른 값들을 가지고 반복 테스트를 하고싶을 때 */
     @DisplayName("스터디 만들기")
     @ParameterizedTest(name="{index} {displayName} message={0}")
-    @ValueSource(ints = {10, 20, 40}) // 숫자를 study 타입으로 변환하고자 함
-    void ParameterizedTest(@ConvertWith(StudyConverter.class) Study study ) { //@ConvertWith로 커스텀한 converter가 어떤것인지 알려두어야 한다 (괄호 안에는 커스텀한 클래스)
-        // SimpleArgumentConverter : 커스텀한 인자값의 타입을 변환해주는 인터페이스 구현체
-        System.out.println(study.getLimit());
+    @CsvSource({"10, '자바 스터디'", "20, 스프링"}) 
+    void ParameterizedTest(@AggregateWith(StudyAggregator.class) Study study) { // 여러 인자값들을 받아서 조합해 Study인스턴스로 만든 테스트 
+        // 2) void ParameterizedTest(ArgumentsAccessor argumentsAccessor ) // ArgumentsAccessor : 인자값을 조합
+        // 1) void ParameterizedTest(Integer limit, String name) - 이렇게 따로 넘겨도 되고, ArgumentsAccessor로 합쳐서 Study로 넘길 수도 있다. 
+        
+       // 2) Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+       // 1) Study study = new Study(limit, name);
+        System.out.println(study);
     }
     
-    // 커스텀한 구현체 만들기
+    /* 커스텀 Accessor 
+     * 제약조건
+     * - 반드시 static 이너 클래스이거나 public 클래스여야 한다*/
+    static class StudyAggregator implements ArgumentsAggregator {
+
+        @Override
+        public Object aggregateArguments(ArgumentsAccessor accessor, ParameterContext context)
+                throws ArgumentsAggregationException {
+            return new Study(accessor.getInteger(0), accessor.getString(1));
+        }
+        
+    }
+    
+    
+    /* 커스텀한 구현체 만들기 
+     * - 하나의 Argument를 다른 타입으로 변환할 때에만 적용되는 컨버터 
+     * -> 두 개라면 @CsvSource */
     static class StudyConverter extends SimpleArgumentConverter{
 
         @Override
