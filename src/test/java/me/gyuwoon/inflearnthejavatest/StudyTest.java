45a29569a0c442e2f6d7121dddd8458bbf09a8ff
gwjung <charminggw@gmail.com>
2020-11-30 06:44:32 +0900

    테스트 순서 @TestMethodOrder - @Order

--- src/test/java/me/gyuwoon/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/gyuwoon/inflearnthejavatest/StudyTest.java
@@ -10,9 +10,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -24,14 +27,28 @@ import org.junit.jupiter.params.converter.ArgumentConversionException;
 import org.junit.jupiter.params.converter.SimpleArgumentConverter;
 import org.junit.jupiter.params.provider.CsvSource;
 
-
+/* 태스트 순서
+ * 내부적으로 정해진 순서가 있긴 하다. 
+ * 하지만 이 순서에 의존해서는 안된다. 
+ * 내부 구현 로직에 따라 언제라도 바뀔 수 있다. 
+ *  "제대로 작성된 유닛테스트라면(하나의 단위라면), 다른 단위테스트와는 독립적으로 실행되어야 한다."
+ *  하지만! 경우에 따라 순서대로 테스트를 작성해야 할 때도 있다. 
+ *   - 통합테스트, 기능테스트, 시나리오 테스트 (유즈케이스를 테스트하는 경우)
+ *
+ * @Order()
+ *  - 인스턴스를 독립적으로 만들지만 실행 순서를 정해줄 수 있다. 
+ *  - @TestInstance(Lifecycle.PER_CLASS)를 쓰지 않아도 괜찮다. 하지만 같이 쓰면 상태 정보도 공유하면서 순차적으로 실행하는 테스트를 만들 수 있다.
+ *  - 단위테스트의 경우는 굳이 순서를 정할 필요는 없을 것 같고, 유즈케이스 기반으로 시나리오 테스트를 만들 때 이용하면 유용.
+ */
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-@TestInstance(Lifecycle.PER_CLASS) // 클래스마다 인스턴스를 생성(클래스당 하나의 인스턴스) - 모든 테스트가 하나의 인스턴스를 공유 -> 성능상의 장점, 제약이 느슨해짐
-class StudyTest { // 기본은 테스트간의 의존성을 없애기 위해 메소드마다(테스트마다) 인스턴스가 새롭게 생성된다. 
+//@TestInstance(Lifecycle.PER_CLASS) 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class) // Orderer를 넘겨준다. 
+class StudyTest {  
     
     
     int value = 1;
 
+    @Order(2)
     @FastTest
     @DisplayName("스터디 만들기 fast")
     void create_new_study() {
@@ -41,13 +58,15 @@ class StudyTest { // 기본은 테스트간의 의존성을 없애기 위해 메
         assertThat(actual.getLimit()).isGreaterThan(0);
     }
 
+    @Order(1) // spring이 제공하는 것 쓰지말고!
     @SlowTest
     @DisplayName("스터디 만들기 slow ")
     void create_new_study_again() {
         System.out.println(this);
         System.out.println("create1 " + value++);
     }
-
+    
+    @Order(3)
     @DisplayName("스터디 만들기")
     @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}") // value: 반복횟수, 
                                                                                               // name : 개별 테스트 이름
@@ -60,6 +79,7 @@ class StudyTest { // 기본은 테스트간의 의존성을 없애기 위해 메
     }
     
     /* ParameterizedTest다른 값들을 가지고 반복 테스트를 하고싶을 때 */
+    @Order(4)
     @DisplayName("스터디 만들기")
     @ParameterizedTest(name="{index} {displayName} message={0}")
     @CsvSource({"10, '자바 스터디'", "20, 스프링"}) 
@@ -101,12 +121,12 @@ class StudyTest { // 기본은 테스트간의 의존성을 없애기 위해 메
     }
     
     @BeforeAll
-     void beforeAll() { // @TestInstance(Lifecycle.PER_CLASS)이면 메서드들간 공유하기 때문에 static일 필요가 없다.
+    static void beforeAll() { // @TestInstance(Lifecycle.PER_CLASS)이면 메서드들간 공유하기 때문에 static일 필요가 없다.
         System.out.println("before all");
     }
 
     @AfterAll
-     void afterAll() {
+    static void afterAll() {
         System.out.println("after all");
     }
 
